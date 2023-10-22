57f3ff42469339137b0cb8b24bff2d3e16eecfc2
Marco Selva <marco.96.12@gmail.com>
2023-10-22 23:42:35 +0200

    update

--- uploader.py
+++ uploader.py
@@ -2,7 +2,10 @@ import os, json
 from datetime import datetime, timedelta
 from re import X
 import pymongo
-import sys
+    
+uploaded = 0
+totalFile = 0
+notUploaded = 0
 
 inputPath = "VALID/01_lay the fav/04_04_2022_16_57_17"
 outputPath = "uploadLog"
@@ -16,15 +19,18 @@ def checkMarketPresentByName(name):
     return db.tradeNew.find({"trade.info.marketInfo.marketName": name})
 
 def uploadInDb(trade):
+    global uploaded
     db.tradeNew.insert_one(trade)
+    uploaded = uploaded+1
     log(f'\t[>] Upload: {marketName}')
 
 def checkByHash(found, trade):
+    global notUploaded
     find = False
     for x in found:
-        del x['_id']
-        if x == trade:
+        if x['trade'] == trade['trade']:
             find = True
+            notUploaded = notUploaded+1
             break
 
     if find:
@@ -37,12 +43,12 @@ logger = open(os.path.join(outputPath, f'{time}_logs.txt'), 'w', encoding='utf-8
 client = pymongo.MongoClient(
     "mongodb+srv://marco:4Nr1fD8mAOSypUur@cluster1.fzsll.mongodb.net/bf_historical?retryWrites=true&w=majority")
 db = client.bf_historical
-    
+
 
 for dirpath, dirnames, filenames in os.walk("./VALID"):
     for filename in [f for f in filenames if f.endswith(".json")]:
         try:
-
+            totalFile = totalFile+1
             name = os.path.join(dirpath, filename)
             log(f'[-] File: {name}')
             file = open(os.path.join(dirpath, filename))
@@ -55,7 +61,10 @@ for dirpath, dirnames, filenames in os.walk("./VALID"):
             else:
                 checkByHash(find, data)
         except Exception as e:
-            log(f"[-] Unable to read Book: {os.path.basename(file)}, Error: {e}")
+            log(f"[-] Unable to read Book: {name}, Error: {e}")
             continue
 
 
+log(f"[v] Total File: {totalFile}")
+log(f"[v] Uploaded File: {uploaded}  ({round(uploaded / totalFile, 2)})")
+log(f"[x] Skipper File: {notUploaded} ({round(notUploaded / totalFile, 2)})")
